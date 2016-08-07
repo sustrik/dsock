680a6da2013480395159bd540e4bc9f26ec7224c
Martin Sustrik <sustrik@250bpm.com>
2016-08-07 08:12:48 +0200

    Bump ABI version
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- dsock.h
+++ dsock.h
@@ -41,7 +41,7 @@
 /*  www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html  */
 
 /*  The current interface version. */
-#define DSOCK_VERSION_CURRENT 0
+#define DSOCK_VERSION_CURRENT 1
 
 /*  The latest revision of the current interface. */
 #define DSOCK_VERSION_REVISION 0
