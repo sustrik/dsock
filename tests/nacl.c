a4f95b185fb6ebe001025cbe1490ca1dcef1a49b
Martin Sustrik <sustrik@250bpm.com>
2016-10-01 17:04:40 +0200

    NaCl test improved
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/nacl.c
+++ tests/nacl.c
@@ -29,7 +29,9 @@
 int main() {
 
     const char key[] = "01234567890123456789012345678901";
+    const char badkey[] = "X1234567890123456789012345678901";
     
+    /* Test normal communication. */
     int s[2];
     int rc = unix_pair(s);
     assert(rc == 0);
@@ -45,14 +47,46 @@ int main() {
     assert(nacl0 >= 0);
     int nacl1 = nacl_start(pfx1, key, 32, -1);
     assert(nacl1 >= 0);
-
     rc = msend(nacl0, "ABC", 3, -1);
+    assert(rc == 0);
+    rc = msend(nacl0, "DEFG", 4, -1);
     assert(rc == 0);
     char buf[10] = {0};
     ssize_t sz = mrecv(nacl1, buf, sizeof(buf), -1);
     assert(sz == 3);
     assert(buf[0] == 'A' && buf[1] == 'B' && buf[2] == 'C');
+    sz = mrecv(nacl1, buf, sizeof(buf), -1);
+    assert(sz == 4);
+    assert(buf[0] == 'D' && buf[1] == 'E' && buf[2] == 'F' && buf[3] == 'G');
+    rc = msend(nacl1, "HIJ", 3, -1);
+    assert(rc == 0);
+    sz = mrecv(nacl0, buf, sizeof(buf), -1);
+    assert(sz == 3);
+    assert(buf[0] == 'H' && buf[1] == 'I' && buf[2] == 'J');
+    rc = hclose(nacl1);
+    assert(rc == 0);
+    rc = hclose(nacl0);
+    assert(rc == 0);
 
+    /* Test communication with wrong key. */
+    rc = unix_pair(s);
+    assert(rc == 0);
+    log0 = blog_start(s[0]);
+    assert(log0 >= 0);
+    log1 = blog_start(s[1]);
+    assert(log1 >= 0);
+    pfx0 = pfx_start(log0);
+    assert(pfx0 >= 0);
+    pfx1 = pfx_start(log1);
+    assert(pfx1 >= 0);
+    nacl0 = nacl_start(pfx0, key, 32, -1);
+    assert(nacl0 >= 0);
+    nacl1 = nacl_start(pfx1, badkey, 32, -1);
+    assert(nacl1 >= 0);
+    rc = msend(nacl0, "ABC", 3, -1);
+    assert(rc == 0);
+    sz = mrecv(nacl1, buf, sizeof(buf), -1);
+    assert(sz == -1 && errno == EACCES);
     rc = hclose(nacl1);
     assert(rc == 0);
     rc = hclose(nacl0);
