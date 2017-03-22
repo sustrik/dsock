6345d264065cab219c267373fee0878607386496
Lev Walkin <vlm@lionet.info>
2017-03-22 13:57:09 -0700

    AES256-SHA support
    Support AES256-SHA CIPHER.

--- dsock.h
+++ dsock.h
@@ -354,6 +354,7 @@ DSOCK_EXPORT int keepalive_detach(
 #define DSOCK_BTLS_CIPHERS_AES128_GCM_SHA256                 (1ull << 36)
 #define DSOCK_BTLS_CIPHERS_AES128_SHA256                     (1ull << 37)
 #define DSOCK_BTLS_CIPHERS_CAMELLIA128_SHA256                (1ull << 38)
+#define DSOCK_BTLS_CIPHERS_AES256_SHA                        (1ull << 39)
 
 #define DSOCK_BTLS_DEFAULT \
     (DSOCK_BTLS_PROTO_DEFAULT| \
